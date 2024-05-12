83d18676c6bc3b7c62a9434ba9ae9bef48439532
Lars Janssen <lars.e.janssen@gmail.com>
2024-05-12 14:39:01 +0200

    fixed server filepath

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -1,31 +1,30 @@
 package server;
 
-import static syslog.Syslog.syslog;
-
 import config.Config;
+
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import static syslog.Syslog.syslog;
+
 public class Server {
 
-  private Config config;
+  private Config config; // TODO change path
 
   private ServerSocket serverSocket;
 
   private ExecutorService pool;
 
-  public Server() throws IOException {
-    try {
-      this.config = new Config().readConfigFromFile("build/resources/main/main/config.json");
-    } catch (Exception e) {
-      // TODO: diffrent path for server
+  public Server () throws IOException {
+    this.config = new Config().readConfigFromFile("src/main/resources/config.json");
+    if (this.config == null) {
+      syslog(1,4,"ERROR: Could not read config, revert to fallback filepath");
+      // TODO
     }
     this.serverSocket = new ServerSocket(config.getPort());
-    pool =
-        Executors.newFixedThreadPool(
-            config.getMaxClients()); // Hält nicht weitere Connection davon ab connectet zu werden.
+    pool = Executors.newFixedThreadPool(config.getMaxClients()); // Hält nicht weitere Connection davon ab connectet zu werden.
   }
 
   public void startServer() {
@@ -34,13 +33,14 @@ public class Server {
         pool.execute(new ClientHandler(serverSocket.accept(), config));
       }
     } catch (IOException e) {
-      syslog(1, 4, "Connection zum Client konnte nicht hergestellt werden");
+      syslog(1,4,"Connection zum Client konnte nicht hergestellt werden");
     }
   }
 
   public static void main(String[] args) throws IOException {
-    syslog(1, 8, "START");
+    syslog(1,8,"START");
     Server server = new Server();
     server.startServer();
   }
+
 }
