fda3f29861777ae1817d03d9990cd3106108fff0
Lars Janssen <lars.e.janssen@gmail.com>
2024-05-21 13:36:04 +0200

    clear

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ rm -rf /rechnernetze
 git clone -b redesign_praktikum1 https://github.com/Lars5Janssen/rechnernetze.git /rechnernetze
 cd /rechnernetze
 ./gradlew fatJar
+clear
 git log -n 1
 cd /rechnernetze/build/libs
 java -cp rechnernetze-1.0-SNAPSHOT.jar server.Server
