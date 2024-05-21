182f7b01e5cab7b15690b0f0971b58bcd5064f5c
Lars Janssen <lars.e.janssen@gmail.com>
2024-05-21 14:52:31 +0200

    dockerfile change

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 service ssh start > /dev/null
 rm -rf /rechnernetze
-git clone -q https://github.com/Lars5Janssen/rechnernetze.git /rechnernetze > /dev/null
+git clone -q -b aufgabe_3 https://github.com/Lars5Janssen/rechnernetze.git /rechnernetze > /dev/null
 cd /rechnernetze
-git log --format=format:"%an --> %Cgreen %B"   -n 1
+git log --format=format:"%an --> %Cgreen%B"   -n 1
 ./gradlew fatJar > /dev/null
 cd /rechnernetze/build/libs
 echo ""
 echo ""
 echo ""
-java -cp rechnernetze-1.0-SNAPSHOT.jar server.Server
+#java -cp rechnernetze-1.0-SNAPSHOT.jar server.Server
+java -cp rechnernetze-1.0-SNAPSHOT.jar server.FileCopyServer
