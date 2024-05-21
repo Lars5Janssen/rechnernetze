f4a9d0db0b517afb25b2c1a0a53fdab4c48b9bb3
Lars Janssen <lars.e.janssen@gmail.com>
2024-05-21 14:03:14 +0200

    thing

--- rebuild.sh
+++ rebuild.sh
@@ -6,5 +6,5 @@ docker build -t snakeoil:latest .
 docker run -d -p 80:4242 --name snakeoil snakeoil:latest
 clear
 rm authorized_keys
-docker exec snakeoil "cd ~/rechnernetze && git log -n 1"
-#docker logs -f --since 0m snakeoil 
+docker exec snakeoil ./gitlog.sh
+docker logs -f --since 0m snakeoil 
