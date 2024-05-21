af10f10d9c82379c38b7e3ea6a0c3bdb417fc36f
Lars Janssen <lars.e.janssen@gmail.com>
2024-05-21 13:39:22 +0200

    a

--- rebuild.sh
+++ rebuild.sh
@@ -5,4 +5,4 @@ docker rmi snakeoil:latest -f
 docker build -t snakeoil:latest .
 docker run -d -p 80:4242 --name snakeoil snakeoil:latest
 rm authorized_keys
-docker logs -f snakeoil
+docker logs -f --since 0m snakeoil 
