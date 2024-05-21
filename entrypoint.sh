03e41af97f79c0dec14809d9d4acac1aaed6b4e4
Lars Janssen <lars.e.janssen@gmail.com>
2024-05-21 14:15:26 +0200

    hmm

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 service ssh start > /dev/null
 rm -rf /rechnernetze
-git clone -b redesign_praktikum1 https://github.com/Lars5Janssen/rechnernetze.git /rechnernetze > /dev/null
+git clone --quite -b redesign_praktikum1 https://github.com/Lars5Janssen/rechnernetze.git /rechnernetze > /dev/null
 cd /rechnernetze
 ./gradlew fatJar > /dev/null
 cd /rechnernetze/build/libs
-git log -n 1
+git log --format=format:"%an --> %Cgreen %B"   -n 1
 java -cp rechnernetze-1.0-SNAPSHOT.jar server.Server
