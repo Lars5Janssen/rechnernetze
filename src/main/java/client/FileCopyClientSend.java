2c4e3dd49b1f43e30e92c6f46babfbd6978b5b91
Lars Janssen <lars.e.janssen@protonmail.com>
2024-06-05 15:06:07 +0200

    change to Byte[] Blockingqueue

--- src/main/java/client/FileCopyClientSend.java
+++ src/main/java/client/FileCopyClientSend.java
@@ -10,12 +10,12 @@ import java.util.concurrent.BlockingQueue;
 
 public class FileCopyClientSend implements Runnable {
     private DatagramSocket socket;
-    private BlockingQueue<FCpacket> queue;
+    private BlockingQueue<byte[]> queue;
     private final int UDP_PACKET_SIZE;
     private final InetAddress SERVER_IP;
     private final int SERVER_PORT;
 
-    public FileCopyClientSend(int UDP_PACKET_SIZE, int SERVER_PORT, DatagramSocket socket, BlockingQueue<FCpacket> queue) {
+    public FileCopyClientSend(int UDP_PACKET_SIZE, int SERVER_PORT, DatagramSocket socket, BlockingQueue<byte[]> queue) {
         this.UDP_PACKET_SIZE = UDP_PACKET_SIZE;
         this.SERVER_IP = InetAddress.getLoopbackAddress();
         this.SERVER_PORT = SERVER_PORT;
@@ -27,17 +27,13 @@ public class FileCopyClientSend implements Runnable {
     @Override
     public void run() {
         while (true) {
-        FCpacket packet;
-        try {
-            packet = queue.take();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+            byte[] packet;
+            try {
+                packet = queue.take();
+                socket.send(new DatagramPacket(packet, UDP_PACKET_SIZE, SERVER_IP, SERVER_PORT));
+            } catch (InterruptedException | IOException e) {
+                throw new RuntimeException(e);
+            }
         }
-        try {
-            socket.send(new DatagramPacket(packet.getSeqNumBytesAndData(), UDP_PACKET_SIZE, SERVER_IP, SERVER_PORT));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
     }
 }
