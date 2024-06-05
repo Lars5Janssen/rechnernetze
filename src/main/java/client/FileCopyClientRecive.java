51f9edbcf3d403438a299df1debfab36354ccd9d
Lars Janssen <lars.e.janssen@protonmail.com>
2024-06-05 16:53:00 +0200

    constructors

--- src/main/java/client/FileCopyClientRecive.java
+++ src/main/java/client/FileCopyClientRecive.java
@@ -17,10 +17,14 @@ public class FileCopyClientRecive implements Runnable{
     private final InetAddress SERVER_IP;
     private final int SERVER_PORT;
 
-    public FileCopyClientRecive(int UDP_PACKET_SIZE, DatagramSocket socket, BlockingQueue<FCpacket> queue) {
+    public FileCopyClientRecive(int UDP_PACKET_SIZE, String SERVER_IP, int SERVER_PORT, DatagramSocket socket, BlockingQueue<FCpacket> queue) throws UnknownHostException {
+        this(UDP_PACKET_SIZE, InetAddress.getByName(SERVER_IP), SERVER_PORT, socket, queue);
+    }
+
+    public FileCopyClientRecive(int UDP_PACKET_SIZE, InetAddress SERVER_IP, int SERVER_PORT, DatagramSocket socket, BlockingQueue<FCpacket> queue) {
         this.UDP_PACKET_SIZE = UDP_PACKET_SIZE;
-        this.SERVER_IP = socket.getInetAddress();
-        this.SERVER_PORT = socket.getPort();
+        this.SERVER_IP = SERVER_IP;
+        this.SERVER_PORT = SERVER_PORT;
         this.socket = socket;
         this.queue = queue;
     }
