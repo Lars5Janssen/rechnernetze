fd71ac993f5c79e499bbfc23a169a88adfdbee7a
Lars Janssen <lars.e.janssen@protonmail.com>
2024-06-05 15:52:47 +0200

    added constructors

--- src/main/java/client/FileCopyClientRecive.java
+++ src/main/java/client/FileCopyClientRecive.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.concurrent.BlockingQueue;
 
 public class FileCopyClientRecive implements Runnable{
@@ -16,9 +17,19 @@ public class FileCopyClientRecive implements Runnable{
     private final InetAddress SERVER_IP;
     private final int SERVER_PORT;
 
-    public FileCopyClientRecive(int UDP_PACKET_SIZE, int SERVER_PORT, DatagramSocket socket, BlockingQueue<FCpacket> queue) {
+    public FileCopyClientRecive(int UDP_PACKET_SIZE, String SERVER_IP, int SERVER_PORT, DatagramSocket socket, BlockingQueue<FCpacket> queue ) throws UnknownHostException {
+        this(UDP_PACKET_SIZE, InetAddress.getByName(SERVER_IP), SERVER_PORT, socket, queue);
+    }
+    public FileCopyClientRecive(int UDP_PACKET_SIZE, byte[] SERVER_IP, int SERVER_PORT, DatagramSocket socket, BlockingQueue<FCpacket> queue ) throws UnknownHostException {
+        this(UDP_PACKET_SIZE, InetAddress.getByAddress(SERVER_IP), SERVER_PORT, socket, queue);
+    }
+
+    public FileCopyClientRecive(int UDP_PACKET_SIZE, int SERVER_PORT, DatagramSocket socket, BlockingQueue<FCpacket> queue ) {
+        this(UDP_PACKET_SIZE, InetAddress.getLoopbackAddress(), SERVER_PORT, socket, queue);
+    }
+    public FileCopyClientRecive(int UDP_PACKET_SIZE, InetAddress SERVER_IP, int SERVER_PORT, DatagramSocket socket, BlockingQueue<FCpacket> queue) {
         this.UDP_PACKET_SIZE = UDP_PACKET_SIZE;
-        this.SERVER_IP = InetAddress.getLoopbackAddress();
+        this.SERVER_IP = SERVER_IP;
         this.SERVER_PORT = SERVER_PORT;
         this.socket = socket;
         this.queue = queue;
--- src/main/java/client/FileCopyClientSend.java
+++ src/main/java/client/FileCopyClientSend.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.concurrent.BlockingQueue;
 
 public class FileCopyClientSend implements Runnable {
@@ -13,9 +14,20 @@ public class FileCopyClientSend implements Runnable {
     private final InetAddress SERVER_IP;
     private final int SERVER_PORT;
 
-    public FileCopyClientSend(int UDP_PACKET_SIZE, int SERVER_PORT, DatagramSocket socket, BlockingQueue<byte[]> queue) {
+    public FileCopyClientSend(int UDP_PACKET_SIZE, String SERVER_IP, int SERVER_PORT, DatagramSocket socket, BlockingQueue<byte[]> queue ) throws UnknownHostException {
+        this(UDP_PACKET_SIZE, InetAddress.getByName(SERVER_IP), SERVER_PORT, socket, queue);
+    }
+    public FileCopyClientSend(int UDP_PACKET_SIZE, byte[] SERVER_IP, int SERVER_PORT, DatagramSocket socket, BlockingQueue<byte[]> queue ) throws UnknownHostException {
+        this(UDP_PACKET_SIZE, InetAddress.getByAddress(SERVER_IP), SERVER_PORT, socket, queue);
+    }
+
+    public FileCopyClientSend(int UDP_PACKET_SIZE, int SERVER_PORT, DatagramSocket socket, BlockingQueue<byte[]> queue ) {
+        this(UDP_PACKET_SIZE, InetAddress.getLoopbackAddress(), SERVER_PORT, socket, queue);
+    }
+
+    public FileCopyClientSend(int UDP_PACKET_SIZE, InetAddress SERVER_IP, int SERVER_PORT, DatagramSocket socket, BlockingQueue<byte[]> queue) {
         this.UDP_PACKET_SIZE = UDP_PACKET_SIZE;
-        this.SERVER_IP = InetAddress.getLoopbackAddress();
+        this.SERVER_IP = SERVER_IP;
         this.SERVER_PORT = SERVER_PORT;
         this.socket = socket;
         this.queue = queue;
